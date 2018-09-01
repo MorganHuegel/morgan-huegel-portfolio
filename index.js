2b21310681612c130389fad862299a6b334999df
MorganHuegel <mhue2910@gmail.com>
2018-09-01 10:55:51 -0400

    updated mobile view to have clipped unicycle picture

--- index.js
+++ index.js
@@ -21,6 +21,7 @@
 // }
 
 function eventListeners(){
+  //Expand more-about-morgan info
   $('.about-me').on('click', '#more-about-morgan', function(){
     const screenWidth = $('body').width();
     if(screenWidth >= 600){
@@ -30,10 +31,14 @@ function eventListeners(){
       $('.about-me-text').css('display', 'inline-block');
       $('.about-me-text').css('width', '65vw');
     }
+    $('.unicycle-pic').css('clip', 'inherit');
+    $('.unicycle-pic-container').css('height', '96vw');
     $('#more-about-morgan').hide();
     $('label[for=more-about-morgan]').hide();
     $('.life-story').slideDown();
   });
+
+  //collapse more-about-moregan info
   $('.about-me').on('click', '#less-about-morgan', function(){
     $('.life-story').slideUp(400, () => {
       const screenWidth = $('body').width();
@@ -42,6 +47,8 @@ function eventListeners(){
         $('.about-me-text').css('padding', '60px 0');
       }
     });
+    $('.unicycle-pic').css('clip', 'rect(2vw, 38vw, 27vw, 14vw)');
+    $('.unicycle-pic-container').css('height', '30vw');
     $('#more-about-morgan').show();
     $('label[for=more-about-morgan]').show();
   });
