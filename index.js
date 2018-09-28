858f39507546a59a0a9810a257dee2cf462046c9
MorganHuegel <mhue2910@gmail.com>
2018-09-27 20:33:41 -0400

    mobile styling using two divs and background image prop

--- index.js
+++ index.js
@@ -1,38 +1,12 @@
 /* global $  */
 'use strict';
 
-// function handleAboutMeResize() {
-//   $(window).resize(function(){
-//     let screenWidth = $('body').width();
-//     console.log(screenWidth);
-//     if(screenWidth >= 600){
-//       $('.about-me-text').css('padding', '60px 0 60px 0');
-//     } else {
-//       $('.about-me-text').css('padding', '10px 0 10px 0');
-//     }
-//     if (screenWidth >= 887 && screenWidth <= 970){
-//       $('.about-me-text').css('display', 'inline-block');
-//       $('.about-me-text').css('width', '65vw');
-//     } else {
-//       $('.about-me-text').css('display', 'initial');
-//       $('.about-me-text').css('width', 'inherent');
-//     }
-//   });
-// }
 
 function eventListeners(){
   //Expand more-about-morgan info
   $('.about-me').on('click', '#more-about-morgan', function(){
     const screenWidth = $('body').width();
-    if(screenWidth >= 600){
-      $('.about-me-text').css('padding', '10px 0 60px 0');
-    }
-    if (screenWidth >= 887 && screenWidth <= 970){
-      $('.about-me-text').css('display', 'inline-block');
-      $('.about-me-text').css('width', '65vw');
-    }
-    $('.unicycle-pic').css('clip', 'initial');
-    $('.unicycle-pic-container').css('height', '96vw');
+    $('.unicycle-pic-height').css('height', '555px');
     $('#more-about-morgan').hide();
     $('label[for=more-about-morgan]').hide();
     $('.life-story').slideDown();
@@ -42,13 +16,9 @@ function eventListeners(){
   $('.about-me').on('click', '#less-about-morgan', function(){
     $('.life-story').slideUp(400, () => {
       const screenWidth = $('body').width();
-      console.log(screenWidth);
-      if(screenWidth >= 600){
-        $('.about-me-text').css('padding', '60px 0');
-      }
     });
     $('.unicycle-pic').css('clip', 'rect(2vw, 38vw, 27vw, 14vw)');
-    $('.unicycle-pic-container').css('height', '30vw');
+    $('.unicycle-pic-height').css('height', '200px');
     $('#more-about-morgan').show();
     $('label[for=more-about-morgan]').show();
   });
