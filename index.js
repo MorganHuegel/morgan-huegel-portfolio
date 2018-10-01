8a84ad278d178ddb34a328da108dd45a0bb36839
MorganHuegel <mhue2910@gmail.com>
2018-09-30 21:46:59 -0400

    updated some content and styling

--- index.js
+++ index.js
@@ -4,25 +4,23 @@
 
 function eventListeners(){
   //Expand more-about-morgan info
-  $('.about-me, .unicycle-pic-width').on('click', '#more-about-morgan, .unicycle-pic-width', function(){
-    const screenWidth = $('body').width();
+  $('.about-me, .unicycle-pic-width').on('click', '#more-about-morgan, .unicycle-pic-width.short', function(){
     $('.unicycle-pic-height').css('height', '555px');
+    $('.unicycle-pic-width').addClass('tall').removeClass('short');
     $('#more-about-morgan').hide();
     $('label[for=more-about-morgan]').hide();
     $('.life-story').slideDown();
   });
 
   //collapse more-about-moregan info
-  $('.about-me').on('click', '#less-about-morgan', function(){
-    $('.life-story').slideUp(400, () => {
-      const screenWidth = $('body').width();
-    });
-    $('.unicycle-pic').css('clip', 'rect(2vw, 38vw, 27vw, 14vw)');
+  $('.about-me').on('click', '#less-about-morgan, .unicycle-pic-width.tall', function(){
+    $('.life-story').slideUp(400);
     $('.unicycle-pic-height').css('height', '200px');
+    $('.unicycle-pic-width').addClass('short').removeClass('tall');
     $('#more-about-morgan').show();
     $('label[for=more-about-morgan]').show();
+
   });
 }
 
 $(eventListeners());
-//$(handleAboutMeResize());
